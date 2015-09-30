c4f0f576eef10ad836507e43f4427fa5c92d0319
anpandu <anpandumail@gmail.com>
2015-09-30 16:13:29 +0700

    BIOLabel: change input data structure

--- modules/BIOLabel.js
+++ modules/BIOLabel.js
@@ -26,24 +26,30 @@ BIOLabel.prototype.label = function(_data) {
   data['labels'] = new Array(tokens.length);
   for (var i = 0; i < data['labels'].length; i++)
     data['labels'][i] = []
-  
-  // concat 'begin' & 'in' to data['labels']
-  for (var label in _data.labels){
-    var labelWords = Tokenizer.tokenize(_data.labels[label]);
-    // get array of match val between text and  label word (true|false)
-    var matchArray = _.map(data['tokens'], function (edata, idata) {
-      var matchLabelWords = _.map(labelWords, function (elw, ilw) { return elw == data['tokens'][idata+ilw]; });
-      return _.every(matchLabelWords, _.identity);
-    });
-    // concat 'begin' & 'in' data['labels'] right now
-    var count = 0;
-    _.each(matchArray, function (match, idx){
-      count = ((match == true)||(count>0 && count<labelWords.length)) ? count+1 : 0;
-      if (count == 1)
-        data['labels'][idx] = data['labels'][idx].concat("b_" + label);
-      else if (count>0 && count<=labelWords.length)
-        data['labels'][idx] = data['labels'][idx].concat("i_" + label);
-    });   
+
+
+  var labels = _data.labels;
+  for (var key in labels){
+    labels[key].forEach(function (label) {
+      var labelString = label;
+      var labelWords = Tokenizer.tokenize(labelString);
+
+      // get array of match val between text and  label word (true|false)
+      var matchArray = _.map(data['tokens'], function (edata, idata) {
+        var matchLabelWords = _.map(labelWords, function (elw, ilw) { return elw == data['tokens'][idata+ilw]; });
+        return _.every(matchLabelWords, _.identity);
+      });
+      
+      // concat 'begin' & 'in' data['labels'] right now
+      var count = 0;
+      _.each(matchArray, function (match, idx){
+        count = ((match == true)||(count>0 && count<labelWords.length)) ? count+1 : 0;
+        if (count == 1)
+          data['labels'][idx] = data['labels'][idx].concat("b_" + key);
+        else if (count>0 && count<=labelWords.length)
+          data['labels'][idx] = data['labels'][idx].concat("i_" + key);
+      });   
+    })
   }
 
   // concat 'other' to data['labels']
--- test/modules/BIOLabel.test.js
+++ test/modules/BIOLabel.test.js
@@ -4,18 +4,18 @@ var assert = require('chai').assert;
 
 var BIOLabel = require('../../modules/BIOLabel.js');
 
-describe('Cleaner', function () {
+describe('BIOLabel', function () {
 
   it('should return bio label from single tag', function () {
     var data = {
-      'text' : 'i eat nasi goreng for breakfast, lunch, and dinner',
-      'labels' : {
-        'food' : 'nasi goreng'
+      text : 'i eat nasi goreng for breakfast, lunch, and dinner',
+      labels : {
+        food : ['nasi goreng']
       }
     };
     var ans = {
-      'tokens' : ['i', 'eat', 'nasi', 'goreng', 'for', 'breakfast', ',', 'lunch', ',', 'and', 'dinner'],
-      'labels' : [['other'], ['other'], ['b_food'], ['i_food'], ['other'], ['other'], ['other'], ['other'], ['other'], ['other'], ['other']]
+      tokens : ['i', 'eat', 'nasi', 'goreng', 'for', 'breakfast', ',', 'lunch', ',', 'and', 'dinner'],
+      labels : [['other'], ['other'], ['b_food'], ['i_food'], ['other'], ['other'], ['other'], ['other'], ['other'], ['other'], ['other']]
     }
     var res = BIOLabel.label(data);
     assert.lengthOf(res['labels'], ans['labels'].length);
@@ -24,15 +24,15 @@ describe('Cleaner', function () {
 
   it('should return bio label from multiple tag', function () {
     var data2 = {
-      'text' : 'i eat nasi goreng at midnight too',
-      'labels' : {
-        'who' : 'i',
-        'what' : 'i eat nasi goreng'
+      text : 'i eat nasi goreng at midnight too',
+      labels : {
+        who : ['i'],
+        what : ['i eat nasi goreng']
       }
     };
     var ans2 = {
-      'tokens' : ['i', 'eat', 'nasi', 'goreng', 'at', 'midnight', 'too'],
-      'labels' : [['b_who', 'b_what'], ['i_what'], ['i_what'], ['i_what'], ['other'], ['other'], ['other']]
+      tokens : ['i', 'eat', 'nasi', 'goreng', 'at', 'midnight', 'too'],
+      labels : [['b_who', 'b_what'], ['i_what'], ['i_what'], ['i_what'], ['other'], ['other'], ['other']]
     }
     var res2 = BIOLabel.label(data2);
     assert.deepEqual(res2['tokens'], ans2['tokens']);
@@ -41,10 +41,10 @@ describe('Cleaner', function () {
       assert.deepEqual(res2.labels[i], ans2.labels[i]);
 
     var data3 = {
-      'text' : 'if you are reading this, you are reading this',
-      'labels' : {
-        'person' : 'you',
-        'activity' : 'you are reading'
+      text : 'if you are reading this, you are reading this',
+      labels : {
+        person : ['you'],
+        activity : ['you are reading']
       }
     };
     var ans3 = { 
@@ -58,9 +58,28 @@ describe('Cleaner', function () {
       assert.deepEqual(res3.labels[i], ans3.labels[i]);
   });
 
+  it('should return bio label from multiple words in one tag', function () {
+    var data = {
+      text : 'friday, saturday, and sunday morning',
+      labels : {
+        day_name : ['friday', 'saturday', 'sunday'],
+        time : ['sunday morning']
+      }
+    };
+    var ans = {
+      tokens : [ 'friday', ',', 'saturday', ',', 'and', 'sunday', 'morning' ],
+      labels : [ ['b_day_name'], ['other'], ['b_day_name'], ['other'], ['other'], ['b_day_name', 'b_time'], ['i_time'] ]
+    }
+    var res = BIOLabel.label(data);
+    assert.deepEqual(res['tokens'], ans['tokens']);
+    assert.lengthOf(res['labels'], ans['labels'].length);
+    for (var i = 0; i < res.labels.length; i++)
+      assert.deepEqual(res.labels[i], ans.labels[i]);
+  })
+
   it('should not return \'other\' BIO label if no \'labels\' field found', function () {
     var data = {};
-    var ans = {'tokens' : [''], 'labels' : [['other']]}
+    var ans = {tokens : [''], labels : [['other']]}
     var res = BIOLabel.label(data);
     assert.lengthOf(res['labels'], ans['labels'].length);
     assert.deepEqual(res, ans);
