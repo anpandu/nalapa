cf668be22308a95df4b8dc5d92c283f00999c040
anpandu <anpandumail@gmail.com>
2015-10-15 11:43:33 +0700

    refactor spaces + tabs

--- test/modules/cleaner.test.js
+++ test/modules/cleaner.test.js
@@ -6,32 +6,32 @@ var cleaner = require('../../modules/cleaner.js');
 
 describe('Cleaner', function () {
 
-    it('isASCII', function () {
-        assert.deepEqual(cleaner.isASCII('abc123'), true);
-        assert.deepEqual(cleaner.isASCII('abc_-8+'), true);
-        assert.deepEqual(cleaner.isASCII('ابت'), false);
-    });
+  it('isASCII', function () {
+    assert.deepEqual(cleaner.isASCII('abc123'), true);
+    assert.deepEqual(cleaner.isASCII('abc_-8+'), true);
+    assert.deepEqual(cleaner.isASCII('ابت'), false);
+  });
 
-    it('isAlphaNumeric', function () {
-        assert.deepEqual(cleaner.isAlphaNumeric('abc123'), true);
-        assert.deepEqual(cleaner.isAlphaNumeric('abc_-8+'), false);
-        assert.deepEqual(cleaner.isAlphaNumeric('ابت'), false);
-    });
+  it('isAlphaNumeric', function () {
+    assert.deepEqual(cleaner.isAlphaNumeric('abc123'), true);
+    assert.deepEqual(cleaner.isAlphaNumeric('abc_-8+'), false);
+    assert.deepEqual(cleaner.isAlphaNumeric('ابت'), false);
+  });
 
-    it('removeNonASCII', function () {
-        assert.deepEqual(cleaner.removeNonASCII('abc123'), 'abc123');
-        assert.deepEqual(cleaner.removeNonASCII('abc_-8+'), 'abc_-8+');
-        assert.deepEqual(cleaner.removeNonASCII('ابت'), '');
-    });
+  it('removeNonASCII', function () {
+    assert.deepEqual(cleaner.removeNonASCII('abc123'), 'abc123');
+    assert.deepEqual(cleaner.removeNonASCII('abc_-8+'), 'abc_-8+');
+    assert.deepEqual(cleaner.removeNonASCII('ابت'), '');
+  });
 
-    it('removeNonAlphaNumeric', function () {
-        assert.deepEqual(cleaner.removeNonAlphaNumeric('abc123'), 'abc123');
-        assert.deepEqual(cleaner.removeNonAlphaNumeric('abc_-8+'), 'abc8');
-        assert.deepEqual(cleaner.removeNonAlphaNumeric('ابت'), '');
-    });
+  it('removeNonAlphaNumeric', function () {
+    assert.deepEqual(cleaner.removeNonAlphaNumeric('abc123'), 'abc123');
+    assert.deepEqual(cleaner.removeNonAlphaNumeric('abc_-8+'), 'abc8');
+    assert.deepEqual(cleaner.removeNonAlphaNumeric('ابت'), '');
+  });
 
-    it('removeHTMLTags', function () {
-        assert.deepEqual(cleaner.removeHTMLTags('<p class="long">some long paragraph</p>'), 'some long paragraph');
-    });
+  it('removeHTMLTags', function () {
+    assert.deepEqual(cleaner.removeHTMLTags('<p class="long">some long paragraph</p>'), 'some long paragraph');
+  });
 
 });
