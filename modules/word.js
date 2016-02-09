1515bc5bedcb56d77d510cf9f25e86dd4a619e50
anpandu <anpandumail@gmail.com>
2016-02-09 17:20:25 +0700

    Word: isBasicWord

--- modules/word.js
+++ modules/word.js
@@ -8,6 +8,12 @@
 
 var _ = require('lodash')
 var stopwords = require('./dictionary/indonesian-stopwords-complete.js')
+var adj = require('./dictionary/adj.js')
+var adv = require('./dictionary/adv.js')
+var num = require('./dictionary/num.js')
+var pre = require('./dictionary/pre.js')
+var pron = require('./dictionary/pron.js')
+var v = require('./dictionary/v.js')
 
 
 /**
@@ -24,8 +30,43 @@ Word.prototype.isStopword = function(word) {
 
 Word.prototype.removeStopwordsFromArray = function(arr, word) {
   return _.filter(arr, function (a) { 
-  	return !Word.prototype.isStopword(a)
+    return !Word.prototype.isStopword(a)
   })
+}
+
+Word.prototype.isAdjective = function(word) {
+  return (adj.indexOf(word) > -1)
+}
+
+Word.prototype.isAdverb = function(word) {
+  return (adv.indexOf(word) > -1)
+}
+
+Word.prototype.isNum = function(word) {
+  return (num.indexOf(word) > -1)
+}
+
+Word.prototype.isPre = function(word) {
+  return (pre.indexOf(word) > -1)
+}
+
+Word.prototype.isPron = function(word) {
+  return (pron.indexOf(word) > -1)
+}
+
+Word.prototype.isVerb = function(word) {
+  return (v.indexOf(word) > -1)
+}
+
+Word.prototype.isBasicWord = function(word) {
+  return (
+    Word.prototype.isAdjective(word) ||
+    Word.prototype.isAdverb(word) ||
+    Word.prototype.isNum(word) ||
+    Word.prototype.isPre(word) ||
+    Word.prototype.isPron(word) ||
+    Word.prototype.isVerb(word)
+  )
 }
 
 module.exports = new Word ()
--- test/modules/word.test.js
+++ test/modules/word.test.js
@@ -17,4 +17,44 @@ describe('Word', function () {
     assert.deepEqual(word.removeStopwordsFromArray(arr), ans)
   })
 
+  it('isAdjective', function () {
+    assert.deepEqual(word.isAdjective('abadi'), true)
+    assert.deepEqual(word.isAdjective('wrongxxx'), false)
+  })
+
+  it('isAdverb', function () {
+    assert.deepEqual(word.isAdverb('adakala'), true)
+    assert.deepEqual(word.isAdverb('wrongxxx'), false)
+  })
+
+  it('isNum', function () {
+    assert.deepEqual(word.isNum('alaf'), true)
+    assert.deepEqual(word.isNum('wrongxxx'), false)
+  })
+
+  it('isPre', function () {
+    assert.deepEqual(word.isPre('adapun'), true)
+    assert.deepEqual(word.isPre('wrongxxx'), false)
+  })
+
+  it('isPron', function () {
+    assert.deepEqual(word.isPron('aku'), true)
+    assert.deepEqual(word.isPron('wrongxxx'), false)
+  })
+
+  it('isVerb', function () {
+    assert.deepEqual(word.isVerb('abdas'), true)
+    assert.deepEqual(word.isVerb('wrongxxx'), false)
+  })
+
+  it('isBasicWord', function () {
+    assert.deepEqual(word.isBasicWord('abadi'), true)
+    assert.deepEqual(word.isBasicWord('adakala'), true)
+    assert.deepEqual(word.isBasicWord('alaf'), true)
+    assert.deepEqual(word.isBasicWord('adapun'), true)
+    assert.deepEqual(word.isBasicWord('aku'), true)
+    assert.deepEqual(word.isBasicWord('abdas'), true)
+    assert.deepEqual(word.isVerb('wrongxxx'), false)
+  })
+
 })
